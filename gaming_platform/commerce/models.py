d2d013a66994e51b29d8c7e427bf8df7dfaa26aa
Abdulmjeed Bojeir <AbdulmjeedJOB12@gmail.com>
2026-04-30 22:55:45 +0300

    cart

--- gaming_platform/commerce/models.py
+++ gaming_platform/commerce/models.py
@@ -11,6 +11,7 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
 
     class Meta:
         unique_together = ('cart', 'game')
