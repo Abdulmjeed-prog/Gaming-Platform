17de6e9d518701ad30138b8c7553599df9936f13
Abdulmjeed Bojeir <AbdulmjeedJOB12@gmail.com>
2026-05-02 14:43:58 +0300

    home page + profile + comment and review

--- gaming_platform/main/views.py
+++ gaming_platform/main/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+from games.models import Game, Genre
 
-# Create your views here.
+def home_view(request: HttpRequest):
+    featured_games = (
+        Game.objects
+        .filter(is_active=True)
+        .select_related('developer')
+        .prefetch_related('genre')
+        .order_by('-created_at')[:4]
+    )
 
-def home_view(request:HttpRequest):
-    if request.user.groups.filter(name='Developer'):
-        print('yes im')
-    return render(request, 'main/home.html')
+    latest_games = (
+        Game.objects
+        .filter(is_active=True)
+        .select_related('developer')
+        .prefetch_related('genre')
+        .order_by('-created_at')[:8]
+    )
+
+    genres = Genre.objects.all().order_by('name')[:6]
+
+    context = {
+        'featured_games': featured_games,
+        'latest_games': latest_games,
+        'genres': genres,
+    }
+    return render(request, 'main/home.html', context)
