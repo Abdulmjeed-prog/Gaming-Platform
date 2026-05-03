e69d0c7c3585683d68d97f02c58b5183bb70c3f1
mansour134 <mansourmasood134@gmail.com>
2026-05-03 22:55:30 +0300

    fix featured games

--- gaming_platform/main/views.py
+++ gaming_platform/main/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpRequest
-from games.models import Game, Genre
+from games.models import Game
 
 
 def home_view(request: HttpRequest):
-    hero_games = (
+    hero_games = list(
         Game.objects
         .filter(is_active=True, is_featured=True)
         .prefetch_related('genre')
-        .order_by('-created_at')[:6]
+        .order_by('-trending_score')[:6]
     )
+
+    if not hero_games:
+        hero_games = list(
+            Game.objects
+            .filter(is_active=True)
+            .prefetch_related('genre')
+            .order_by('-created_at')[:6]
+        )
 
     trending_games = (
         Game.objects
--- gaming_platform/social/signals.py
+++ gaming_platform/social/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.db.models import Avg, Count
 from .models import Review
+from games.utils import bump_trending_score
 
 
 def _update_game_rating(game):
@@ -15,10 +16,13 @@ def _update_game_rating(game):
 
 
 @receiver(post_save, sender=Review)
-def review_saved(sender, instance, **kwargs):
+def review_saved(sender, instance, created, **kwargs):
     _update_game_rating(instance.game)
+    if created:
+        bump_trending_score(instance.game, 1)
 
 
 @receiver(post_delete, sender=Review)
 def review_deleted(sender, instance, **kwargs):
     _update_game_rating(instance.game)
+    bump_trending_score(instance.game, -1)
